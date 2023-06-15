b2c708758dcf01db857845455d9681dc4abc00a7
Indri <indriririn602@gmail.com>
2023-06-15 23:17:40 +0700

    Second Commit

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/project")
+def project():
+    return render_template("project.html")
+
+if __name__ == '__main__':
+    app.run(debug=True) #host="0.0.0.0", port=8000
